a9287aec2f8f51fa271fbb6da71410724e4357c2
Matvey <matveymikhin@gmail.com>
2024-10-12 15:10:58 +0300

    Задание7

---
+++ Course/2nd year/Lesson8/PracticeAB/Task7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7
+{
+    internal class Program
+    {
+        static int vow(string s)
+        {
+            int k = 0;
+            string vowels = "аиоуеэАИОУЕЭ";
+            foreach (char c in s)
+            {
+                if (vowels.Contains(c)) { 
+                    k++; 
+                }
+            }
+            return k;
+        }
+        static void Main(string[] args)
+        {
+            string s = "ааабббффоЭУ";
+            Console.WriteLine(vow(s));
+        }
+    }
+}
