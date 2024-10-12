8cd4dde7696e9bf8a19540acc02161616f1c228c
Matvey <matveymikhin@gmail.com>
2024-10-12 15:01:19 +0300

    Задание5

---
+++ Course/2nd year/Lesson8/PracticeAB/Task5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    internal class Program
+    {
+        static int salary(int x)
+        {
+            return x * 12;
+        }
+        static void Main(string[] args)
+        {
+            int x = 50000;
+            Console.WriteLine(salary(x));
+        }
+    }
+}
