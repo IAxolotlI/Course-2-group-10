4261da25a85b0126fe8b7c3e56132c1dd6d29782
Matvey <matveymikhin@gmail.com>
2024-10-12 14:58:48 +0300

    Задание4

---
+++ Course/2nd year/Lesson8/PracticeAB/Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    internal class Program
+    {
+        static int maxArr(int[] a)
+        {
+            return a.Max(); 
+        }
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3, 4, 7 };
+            Console.WriteLine(maxArr(a));
+        }
+    }
+}
