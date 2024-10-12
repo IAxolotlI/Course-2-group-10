f25c745e0e98af4c9d382ad10613e2cebeb86b46
Matvey <matveymikhin@gmail.com>
2024-10-12 15:05:52 +0300

    Задание6

---
+++ Course/2nd year/Lesson8/PracticeAB/Task6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    internal class Program
+    {
+        static int conv_c_to_f(int x)
+        {
+            return x * 9 / 5 + 32;
+        }
+        static void Main(string[] args)
+        {
+            int x = 25;
+            Console.WriteLine(conv_c_to_f(x));
+        }
+    }
+}
