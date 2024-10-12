dc3bf67891144aa8105ba5ad88f47c8b214bcab8
Matvey <matveymikhin@gmail.com>
2024-10-12 14:58:17 +0300

    Задание1

---
+++ Course/2nd year/Lesson8/PracticeAB/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static bool is_even(int x)
+        {
+            return x % 2 == 0;
+        }
+        static void Main(string[] args)
+        {
+            int a = 5;
+            Console.WriteLine(is_even(a));
+        }
+    }
+}
