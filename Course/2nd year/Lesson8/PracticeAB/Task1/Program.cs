b0b1c3680cf11e10bcc48a12da14092514189342
Matvey <matveymikhin@gmail.com>
2024-10-12 14:53:12 +0300

    Задание1

---
+++ Course/2nd year/Lesson8/PracticeAB/Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+            int a = 6, b = 7;
+            Console.WriteLine(Add(a,b));
+        }
+    }
+}
