adeb866c700a705fd10f68f255fc3cb7f126bce0
Matvey <matveymikhin@gmail.com>
2024-10-12 14:58:32 +0300

    Задание3

---
+++ Course/2nd year/Lesson8/PracticeAB/Task3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static string reverse_string(string s)
+        {
+            string reversed="";
+            for (int i = s.Length - 1; i >= 0; --i)
+            {
+                reversed += s[i];
+            }
+            return reversed;
+        }
+  
+        static void Main(string[] args)
+        {
+            string a = "123456";
+            Console.WriteLine(reverse_string(a));
+        }
+    }
+}
