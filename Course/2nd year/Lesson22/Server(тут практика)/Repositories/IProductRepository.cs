3facfa8a89596b5b940a4b6f61d7271eb9c097d7
Matvey <matveymikhin@gmail.com>
2024-12-20 20:03:59 +0300

    Program

---
+++ Course/2nd year/Lesson22/Server(тут практика)/Repositories/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using PracticeA.Classes;
+
+namespace PracticeA.Repositories
+{
+    public interface IProductRepository
+    {
+        public void Update_Price(string name, double newPrice);
+        public void Update_Name(string name, string newName);
+        public List<Product>? OutOfStock();
+        public void AddProduct(Product product);
+        public void DeleteProduct(string name);
+        public List<Product> GetAllProducts();
+    }
+}
---
+++ Course/2nd year/Lesson22/Server(тут практика)/Repositories/ProductRepository.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PracticeA.Classes;
+using System.Xml.Linq;
+
+namespace PracticeA.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private List<Product>? _products;
+        private ClassDB _classDB;
+        public ProductRepository()
+        {
+            _classDB.Read_Data_From_DB();
+        }
+        public void Update_Price(string name, double newPrice)
+        {
+            if (_products != null)
+            {
+                var product = _products.FirstOrDefault(p => p.Name == name);
+                if (product != null)
+                {
+                    product.Price = newPrice;
+                    SaveChanges();
+                }
+                else
+                {
+                    throw new KeyNotFoundException();
+                }
+            }
+            else
+            {
+                throw new NullReferenceException();
+            }
+        }
+
+        public void Update_Name(string name, string newName)
+        {
+            if (_products != null)
+            {
+                var product = _products.FirstOrDefault(p => p.Name == name);
+                if (product != null)
+                {
+                    product.Name = newName;
+                    SaveChanges();
+                }
+                else
+                {
+                    throw new KeyNotFoundException();
+                }
+            }
+            else
+            {
+                throw new NullReferenceException();
+            }
+        }
+
+        public List<Product>? OutOfStock()
+        {
+            if (_products != null)
+            {
+                var outOfStockItems = _products.Where(p => p.Stock == 0).ToList();
+                if (outOfStockItems.Any())
+                {
+                    return outOfStockItems;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                throw new NullReferenceException();
+            }
+        }
+
+        public void AddProduct(Product product)
+        {
+            _products.Add(product);
+            SaveChanges();
+        }
+        public void DeleteProduct(string name)
+        {
+            var product = _products.FirstOrDefault(p => p.Name == name);
+            if (product != null)
+            {
+                _products.Remove(product);
+                SaveChanges();
+            }
+            else
+            {
+                throw new KeyNotFoundException();
+            }
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            if (_products != null)
+            {
+                return _products;
+            }
+            else
+            {
+                throw new KeyNotFoundException();
+            }
+        }
+
+        private void SaveChanges()
+        {
+            _classDB.WriteDataToFile();
+        }
+    }
+}
